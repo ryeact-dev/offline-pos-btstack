608d5dfe8695b44b0c02606c1f2a02a09aa5e654
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-08 15:21:11 +0800

    Remove default button

--- apps/web/src/components/nav-main.tsx
+++ apps/web/src/components/nav-main.tsx
@@ -1,6 +1,7 @@
-import { IconCirclePlusFilled, IconMail, type Icon } from '@tabler/icons-react';
+import { Link } from '@tanstack/react-router';
 
-import { Button } from '@/components/ui/button';
+import type { Icon } from '@tabler/icons-react';
+
 import {
   SidebarGroup,
   SidebarGroupContent,
@@ -23,30 +24,20 @@ export function NavMain({
     <SidebarGroup>
       <SidebarGroupContent className='flex flex-col gap-2'>
         <SidebarMenu>
-          <SidebarMenuItem className='flex items-center gap-2'>
-            <SidebarMenuButton
-              tooltip='Quick Create'
-              className='bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear'
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-            <Button
-              size='icon'
-              className='size-8 group-data-[collapsible=icon]:opacity-0'
-              variant='outline'
-            >
-              <IconMail />
-              <span className='sr-only'>Inbox</span>
-            </Button>
-          </SidebarMenuItem>
-        </SidebarMenu>
-        <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
+              <SidebarMenuButton tooltip={item.title} asChild>
+                <Link
+                  to={item.url}
+                  search={item.search}
+                  activeProps={{
+                    className:
+                      'bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear',
+                  }}
+                >
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
