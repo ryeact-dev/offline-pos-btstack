f8641ed5d0e01326aaeb9ed6d4cc5b67aa230e2f
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-09 20:15:46 +0800

    update type validations

--- apps/web/src/zod/table.validation.ts
+++ apps/web/src/zod/table.validation.ts
@@ -8,6 +8,23 @@ const dashboardTableSchema = z.object({
   target: z.string(),
   limit: z.string(),
   reviewer: z.string(),
+  // text: z.string(),
 });
 
 export type DashboardTable = z.infer<typeof dashboardTableSchema>;
+
+const inventoryTableSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  price: z.number(),
+  description: z.string(),
+  category: z.string(),
+  stockQuantity: z.string(),
+  image64Base: z.string(),
+  sku: z.string(),
+  barcode: z.string(),
+  expirationDate: z.string(),
+  deliveryDate: z.string(),
+});
+
+export type InventoryTable = z.infer<typeof inventoryTableSchema>;
