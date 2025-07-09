36f50383b0b14f46d1b9d8a931b5a7b8fca9ccfc
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-09 20:14:02 +0800

    add drawer components

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -14,6 +14,7 @@ import type { QueryClient } from '@tanstack/react-query';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { SiteHeader } from '@/components/site-header';
 import { AppSidebar } from '@/components/app-sidebar';
+import DrawerContainer from '@/components/drawer-container';
 
 export interface RouterAppContext {
   queryClient: QueryClient;
@@ -70,6 +71,8 @@ function RootDocument() {
                 <SiteHeader />
                 <Outlet />
               </SidebarInset>
+
+              <DrawerContainer />
             </SidebarProvider>
           )}
         </div>
