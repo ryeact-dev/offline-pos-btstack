9afa5cebbecd39ff04240272e9c430995ba59381
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-09 16:43:09 +0800

    add zod table validation

---
+++ apps/web/src/zod/table.validation.ts
@@ -0,0 +1,13 @@
+import z from 'zod';
+
+const dashboardTableSchema = z.object({
+  id: z.number(),
+  header: z.string(),
+  type: z.string(),
+  status: z.string(),
+  target: z.string(),
+  limit: z.string(),
+  reviewer: z.string(),
+});
+
+export type DashboardTable = z.infer<typeof dashboardTableSchema>;
