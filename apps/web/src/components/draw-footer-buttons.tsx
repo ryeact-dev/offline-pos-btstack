8d09ebc6d4944077d436a2a8e10e8ba898f04178
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-10 17:34:02 +0800

    add loading state on buttons

--- apps/web/src/components/draw-footer-buttons.tsx
+++ apps/web/src/components/draw-footer-buttons.tsx
@@ -3,15 +3,24 @@ import { Button } from './ui/button';
 import { closeSheet } from '@/store';
 import { IconSend, IconX } from '@tabler/icons-react';
 
-export default function DrawerFooterButtons() {
+export default function DrawerFooterButtons({
+  isLoading,
+}: {
+  isLoading: boolean;
+}) {
   return (
     <DrawerFooter className='p-0'>
-      <Button type='submit'>
+      <Button type='submit' disabled={isLoading}>
         <IconSend className='size-4' />
         Submit
       </Button>
       <DrawerClose asChild>
-        <Button type='button' onClick={closeSheet} variant='outline'>
+        <Button
+          type='button'
+          onClick={closeSheet}
+          disabled={isLoading}
+          variant='outline'
+        >
           <IconX className='size-4' /> Close
         </Button>
       </DrawerClose>
