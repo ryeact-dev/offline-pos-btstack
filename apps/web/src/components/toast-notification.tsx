b1a18620c8ab7939cf7a4b5801e8b8c05a50be37
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-10 17:33:47 +0800

    add toast notification helper

---
+++ apps/web/src/components/toast-notification.tsx
@@ -0,0 +1,30 @@
+import { toast } from 'sonner';
+
+type ToastType = 'success' | 'error' | 'info' | 'warning';
+
+interface ToastNotificationProps {
+  title: string;
+  description: string;
+  toastType?: ToastType;
+}
+
+export const ToastNotification = ({
+  title,
+  description,
+  toastType = 'success',
+}: ToastNotificationProps) => {
+  return toast[toastType](title, { description, duration: 3000 });
+
+  // switch (toastType) {
+  //   case 'success':
+  //     return toast.success(message, { toastId, duration: 3000 });
+  //   case 'error':
+  //     return toast.error(message, { toastId, duration: 3000 });
+  //   case 'info':
+  //     return toast.info(message, { toastId, duration: 3000 });
+  //   case 'warning':
+  //     return toast.warning(message, { toastId, duration: 3000 });
+  //   default:
+  //     return;
+  // }
+};
