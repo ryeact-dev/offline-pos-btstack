b77e3573c6af4da814bc628c0eb4474b16eb89da
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-10 12:35:09 +0800

    add button icons

--- apps/web/src/components/draw-footer-buttons.tsx
+++ apps/web/src/components/draw-footer-buttons.tsx
@@ -1,14 +1,18 @@
 import { DrawerClose, DrawerFooter } from './ui/drawer';
 import { Button } from './ui/button';
 import { closeSheet } from '@/store';
+import { IconSend, IconX } from '@tabler/icons-react';
 
 export default function DrawerFooterButtons() {
   return (
-    <DrawerFooter>
-      <Button>Submit</Button>
+    <DrawerFooter className='p-0'>
+      <Button type='submit'>
+        <IconSend className='size-4' />
+        Submit
+      </Button>
       <DrawerClose asChild>
-        <Button onClick={closeSheet} variant='outline'>
-          Done
+        <Button type='button' onClick={closeSheet} variant='outline'>
+          <IconX className='size-4' /> Close
         </Button>
       </DrawerClose>
     </DrawerFooter>
