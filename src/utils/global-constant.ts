e232c5fe5ce49c24b7aa32f7a44a9df595acd6bb
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-12 11:28:09 +0800

    update product category

--- src/utils/global-constant.ts
+++ src/utils/global-constant.ts
@@ -1,11 +1,9 @@
-export const PRODUCT_CATEGORY = [
-  { value: 'medicine', label: 'Medicine' },
-  { value: 'food', label: 'Food' },
-  { value: 'clothing', label: 'Clothing' },
-  { value: 'electronics', label: 'Electronics' },
-  { value: 'household', label: 'Household' },
-  { value: 'toys', label: 'Toys' },
-  { value: 'other', label: 'Other' },
+export const PRODUCT_UNIT = [
+  { value: "pieces", label: "Pieces" },
+  { value: "boxes", label: "Boxes" },
+  { value: "bottles", label: "Bottles" },
+  { value: "cans", label: "Cans" },
+  { value: "cartons", label: "Cartons" },
 ];
 
-export const TEMP_USER_ID = '75a7fd35-2358-4b09-a983-06b7d2b557fa';
+export const TEMP_USER_ID = "75a7fd35-2358-4b09-a983-06b7d2b557fa";
