6f018869e5f71f7b63839bc9c20d726bee7d8971
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-15 17:22:41 +0800

    add resolvers fro tabler icons

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,11 @@ export default defineConfig(async ({ mode }) => {
       }),
       viteReact(),
     ],
+    resolve: {
+      alias: {
+        // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+        "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+      },
+    },
   };
 });
