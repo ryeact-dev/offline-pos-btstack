8f0f84de303c94e872c28f980429f4c6dffa5ffe
ryeact-dev <rmontoya.umtc@gmail.com>
2025-07-24 14:25:34 +0800

    finish checkout component

--- src/components/modals/check-out-order.tsx
+++ src/components/modals/check-out-order.tsx
@@ -1,8 +1,30 @@
 import type { OrderDetails } from "@/utils/types";
-import React from "react";
 import ModalFooterButtons from "../modal-footer-buttons";
 import { Separator } from "../ui/separator";
+import { pesoPriceFormat } from "@/helpers/client/price-formats";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  checkOutOrderSchema,
+  type CheckOutOrderValues,
+} from "@/zod/products.validation";
+import { useCheckOutSalesMutation } from "@/hooks/cart.hooks";
 // import { useCheckOutSalesMutation } from "@/hooks/cart.hooks";
+
+const DEFAULT_FORM_VALUES = {
+  id: 0,
+  customerName: "",
+  userName: "",
+};
 
 export default function CheckOutOrder({
   data: item,
@@ -11,30 +33,59 @@ export default function CheckOutOrder({
   data: OrderDetails;
   onClose: () => void;
 }) {
-  // const { mutate: addSalesMutate, isPending: isAddingSales } =
-  // useCheckOutSalesMutation(onClose);
+  const form = useForm<CheckOutOrderValues>({
+    resolver: zodResolver(checkOutOrderSchema),
+    defaultValues: item ? item : DEFAULT_FORM_VALUES,
+  });
 
-  // const itemCount = useCartStore((state) => state.itemCount());
+  const { mutate: checkOutMutate, isPending } =
+    useCheckOutSalesMutation(onClose);
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // addSalesMutate(item);
-    //   onClose();
+  const onSubmit = (values: CheckOutOrderValues) => {
+    console.log(values);
+    checkOutMutate(values);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <h2 className="mb-1 text-xl font-medium">
-          {/* Check out {itemCount} items? */}
-        </h2>
-        {/* <Separator className="my-3" /> */}
-        <p>Subtotal: P{item.subTotal.toFixed(2)}</p>
-        <p>Tax: P{item.tax.toFixed(2)}</p>
-        <Separator className="my-3" />
-        <p>Total: P{item.totalWithTax.toFixed(2)}</p>
-      </div>
-      <ModalFooterButtons isLoading={false} />
-    </form>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        // className='h-[98%] flex flex-col justify-between'
+        className="space-y-6"
+      >
+        <div className="space-y-4">
+          <div>
+            <h2 className="mb-1 text-xl font-medium">
+              {/* Check out {itemCount} items? */}
+            </h2>
+            {/* <Separator className="my-3" /> */}
+            <p>Subtotal: {pesoPriceFormat(item.subTotal.toString())}</p>
+            <p>Tax: {pesoPriceFormat(Number(item.tax).toFixed(2))}</p>
+            <Separator className="my-3" />
+            <p className="text-lg font-semibold tracking-wider">
+              Total: {pesoPriceFormat(item.totalWithTax.toString())}
+            </p>
+          </div>
+        </div>
+
+        <FormField
+          control={form.control}
+          name="customerName"
+          render={({ field }) => (
+            <FormItem className="col-span-3">
+              <FormLabel>Customer Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Product Name" {...field} />
+              </FormControl>
+              {/* <FormDescription>{null}</FormDescription> */}
+              <div className="h-6">
+                <FormMessage />
+              </div>
+            </FormItem>
+          )}
+        />
+        <ModalFooterButtons isLoading={isPending} />
+      </form>
+    </Form>
   );
 }
